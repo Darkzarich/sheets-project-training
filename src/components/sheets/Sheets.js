acbcab8012eccb8386847dabcc76291fe219b8d6
Darkzarich <darkzarichv2@gmail.com>
2021-04-15 04:30:12 +0300

    start making an util to work with DOM easier

--- src/components/sheets/Sheets.js
+++ src/components/sheets/Sheets.js
@@ -1,18 +1,18 @@
+import {$} from '@engine/EngineDOM'
+
 export class Sheets {
   constructor(selector, options) {
-    this.$el = document.querySelector(selector)
+    this.$el = $(selector)
     this.components = options.components || []
   }
 
   getRoot() {
-    const $root = document.createElement('div')
-    $root.classList.add('l-sheets')
+    const $root = $.create('div', 'l-sheets')
 
     this.components.forEach((Component) => {
-      const $el = document.createElement('div')
-      $el.classList.add(Component.className)
+      const $el = $.create('div', Component.className)
       const component = new Component($el)
-      $el.innerHTML = component.toHTML()
+      $el.html(component.toHTML())
       $root.append($el)
     })
 
