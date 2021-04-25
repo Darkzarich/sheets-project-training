b772a71e0497d927a3ade2a7cd85c575e1ba754f
Darkzarich <darkzarichv2@gmail.com>
2021-04-25 14:27:48 +0300

    fix addClass & removeClass return

--- src/engine/EngineDOM.js
+++ src/engine/EngineDOM.js
@@ -93,11 +93,13 @@ class EngineDOM {
   }
 
   addClass(className) {
-    return $(this.$el.classList.add(className))
+    this.$el.classList.add(className)
+    return this
   }
 
   removeClass(className) {
-    return $(this.$el.classList.remove(className))
+    this.$el.classList.remove(className)
+    return this
   }
 
   find(selector) {
