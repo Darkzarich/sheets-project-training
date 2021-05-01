31357a165f501e65361676cc959738be0725baf2
Darkzarich <darkzarichv2@gmail.com>
2021-05-01 21:09:34 +0300

    create basic store

--- src/index.js
+++ src/index.js
@@ -5,9 +5,14 @@ import { Header } from '@/components/header/Header'
 import { Toolbar } from '@/components/toolbar/Toolbar'
 import { Formula } from '@/components/formula/Formula'
 import { Table } from '@/components/table/Table'
+import { createStore } from '@engine/createStore'
+import { rootReducer } from './store/rootReducer'
+
+const store = createStore(rootReducer)
 
 const sheets = new Sheets('#app', {
   components: [Header, Toolbar, Formula, Table],
+  store,
 })
 
 sheets.render()
