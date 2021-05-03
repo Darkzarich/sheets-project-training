ec6853a68a08e3e248b38a8c9ea3c57e2192df53
Darkzarich <darkzarichv2@gmail.com>
2021-05-03 10:44:42 +0300

    add jsdoc for utils functions

--- src/engine/utils.js
+++ src/engine/utils.js
@@ -1,5 +1,9 @@
 // Pure functions
 
+/**
+ * @param {string} string
+ * @return {string} the first letter of string uppercased
+ */
 export const capitalize = (string) => {
   if (typeof string !== 'string') {
     return ''
@@ -7,6 +11,12 @@ export const capitalize = (string) => {
   return string.charAt(0).toUpperCase() + string.slice(1)
 }
 
+/**
+ * Swaps props values if provided key prop is less for obj2 compared to obj1
+ * @param {Object} obj1
+ * @param {Object} obj2
+ * @param {string} prop
+ */
 export const swapPropsIfLess = (obj1, obj2, prop) => {
   if (obj1[prop] > obj2[prop]) {
     const temp = obj1[prop]
@@ -15,6 +25,12 @@ export const swapPropsIfLess = (obj1, obj2, prop) => {
   }
 }
 
+/**
+ * Sets localStorage key to data. if no data provided gets the value from the key
+ * @param {string} key
+ * @param {Object} data
+ * @return {Object} localStorage parsed value
+ */
 export const storage = (key, data = null) => {
   if (!data) {
     return JSON.parse(localStorage.getItem(key))
@@ -22,6 +38,11 @@ export const storage = (key, data = null) => {
   localStorage.setItem(key, JSON.stringify(data))
 }
 
+/**
+ * Returns HTML-valid styles string from an object. Ensures kebab-case
+ * @param {Object} styleObj
+ * @return {string} HTML-valid styles string
+ */
 export const toCSS = (styleObj = {}) => {
   return Object.keys(styleObj)
     .map((key) =>
@@ -30,10 +51,21 @@ export const toCSS = (styleObj = {}) => {
     .join('')
 }
 
+/**
+ * Transforms camelCase string to kebab-case
+ * @param {string} str
+ * @return {string} kebab-case string
+ */
 export const fromCamelToKebab = (str) => {
   return str.replace(/[A-Z]/g, (m) => '-' + m.toLowerCase())
 }
 
+/**
+ * Compare two any values. For objects compare their stringify versions
+ * @param {*} a
+ * @param {*} b
+ * @return {boolean}
+ */
 export const isEqual = (a, b) => {
   if (typeof a === 'object' && typeof b === 'object') {
     return JSON.stringify(a) === JSON.stringify(b)
